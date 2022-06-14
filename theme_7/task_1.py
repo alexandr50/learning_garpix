9d06de878b27ae0b03b07a4b27d6cac7669b52e7
alexandr50 <dmitrtymaslov2016@mail.ru>
2022-06-14 17:20:34 +0300

    homework for theme_7

--- theme_7/task_1.py
+++ theme_7/task_1.py
@@ -1,5 +1,5 @@
 
-with open('folder/file.txt', 'r', encoding='utf-8') as file:
-    res = file.readlines()
+with open('folder/file.txt', 'r', encoding='utf-8') as f:
+    res = f.readlines()
     result = [i.strip() for i in res]
     print(*result[:6], sep='\n')
