a3c4b66d89735759ddf9c6030a30e5b87d3cac42
alexandr50 <dmitrtymaslov2016@mail.ru>
2022-06-19 12:31:29 +0300

    changes for theme

--- theme_4/task_2.py
+++ theme_4/task_2.py
@@ -2,6 +2,5 @@ list1 = [5, 10, 15, 20, 25, 20, 50]
 
 for i in range(len(list1)):
     if list1[i] == 20:
-        list1.pop(i)
-        list1.insert(i, 200)
+        list1[i] = 200
 print(list1)
--- theme_4/task_5.py
+++ theme_4/task_5.py
@@ -1,14 +1,19 @@
 lst1 = ['Санкт-Петербург', 'Москва', 'Казань']
 lst2= ['Воронеж', 'Санкт-Петербург', 'Иваново']
 # список городов которые есть в обоих списках
-result1 = []
-for i in lst1:
-    if i in lst2:
-        result1.append(i)
-print(result1)
-# список с городами которые есть в одном из списков
-result2 = lst1 + lst2
-for i in result2:
-    if result2.count(i) > 1:
-        result2.remove(i)
-print(result2)
+# result1 = []
+# for i in lst1:
+#     if i in lst2:
+#         result1.append(i)
+# print(result1)
+# # список с городами которые есть в одном из списков
+# result2 = lst1 + lst2
+# for i in result2:
+#     if result2.count(i) > 1:
+#         result2.remove(i)
+# print(result2)
+
+# решение с помощью множеств
+res1 = set(lst1) & set(lst2)
+res2 = set(lst1) ^ set(lst2)
+print(res1, res2)
