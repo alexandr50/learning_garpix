1c1138b3d2c2edd7ad998f042e528af3c117e8da
alexandr50 <dmitrtymaslov2016@mail.ru>
2022-06-19 12:04:05 +0300

    changes for theme_1_task_1

--- theme_1/task_1.py
+++ theme_1/task_1.py
@@ -1,3 +1,3 @@
 initials = input('Введите ФИО полностью: ')
 address = input('Введите адрес: ')
-print(f'"Здравствуйте, {initials}. Ваш заказ отправлен по адресу {address}')
+print(f'"Здравствуйте, {initials.title()}. Ваш заказ отправлен по адресу {address.title()}')
